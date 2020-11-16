4c0ab876a0dda84fcf25b32917907d9373a2a89c
anil ekici <anilcanekici@gmail.com>
2020-11-16 16:04:14 +0000

    gift model created

--- app/models/pot.rb
+++ app/models/pot.rb
@@ -1,5 +1,6 @@
 class Pot < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
+  has_many :gifts
   
   validates :name, presence :true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :pots
+  validates :first_name, :last_name, :email, presence: true
 end
