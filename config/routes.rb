918e21dd1f444e470d6780a03c5b0e6f8d0abb92
Marta Simas <azevedo_m@outlook.com>
2020-11-17 15:08:40 +0000

    gifts controller completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pots do 
-    resources :gifts, only: [:new, :create, :index]
+  resources :pots, except: [:destroy] do 
+    resources :gifts, only: [:new, :create, :index, :destroy]
   end
   resources :gifts, only: [:edit, :update]
 end
