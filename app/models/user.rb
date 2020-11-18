582fd1a92232eaeff0fa4561fa2542977317af72
anil ekici <anilcanekici@gmail.com>
2020-11-18 12:35:55 +0000

    model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :pots
+  has_many :owned_pots, class_name: 'Pot'
+
   has_many :votes
+
+  has_many :users_pots
+  has_many :pots, through: :users_pots
+
   validates :first_name, :last_name, :email, presence: true
 end
