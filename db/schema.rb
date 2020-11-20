8ca99932639e7bbc24ab4cab96279b85869af261
Marta Simas <azevedo_m@outlook.com>
2020-11-20 13:08:02 +0000

    coutdown done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_120851) do
+ActiveRecord::Schema.define(version: 2020_11_20_123416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 2020_11_19_120851) do
   create_table "pots", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
-    t.datetime "start_date"
-    t.datetime "end_date"
+    t.date "start_date"
+    t.date "end_date"
     t.integer "min_amount", default: 1, null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
