ff37a5489a6d1b90fa2738422bde8ff2503de688
TJ Hollis <thewelshaussie@hotmail.com>
2020-11-20 16:45:40 +0000

    reinstate name validation

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -2,7 +2,7 @@ class Gift < ApplicationRecord
   belongs_to :pot
   has_many :gift_votes
 
-  #validates :name, presence: true
+  validates :name, presence: true
 
   def votes
     gift_votes.count
