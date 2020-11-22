8ee1b10fa222421d12ba97b4172aeeed5a2a7cd6
TJ Hollis <thewelshaussie@hotmail.com>
2020-11-22 18:25:35 +0000

    error in contribute path

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,2 +1,22 @@
 class ContributionsController < ApplicationController
+  before_action :set_pot, only: [:create, :show]
+
+  def new
+    @contribution = Contribution.new
+  end
+
+  def create
+    @pot = Pot.find(params[:pot_id])
+    @contribution = Contribution.new(contribution_params)
+  end
+
+  private
+
+  def contribution_params
+    params.require(:contribution).permit(:pot_id, :name)
+  end
+
+  def set_pot
+    @pot = Pot.find(params[:id])
+  end
 end
