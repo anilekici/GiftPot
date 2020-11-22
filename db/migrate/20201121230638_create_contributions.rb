1939615f32285ef2e16248f6efc0869020c64c6e
TJ Hollis <thewelshaussie@hotmail.com>
2020-11-22 17:06:05 +0000

    fixed syntax error in migration

--- db/migrate/20201121230638_create_contributions.rb
+++ db/migrate/20201121230638_create_contributions.rb
@@ -2,7 +2,7 @@ class CreateContributions < ActiveRecord::Migration[6.0]
   def change
     create_table :contributions do |t|
       t.string :contribution_sku
-      t.monetize :amount currency: { present: false }
+      t.monetize :amount, currency: { present: false }
       t.string :session_id
       t.references :user, null: false, foreign_key: true
       t.references :pot, null: false, foreign_key: true
