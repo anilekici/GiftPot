bd6f312ea903a23c6557db30af443d1d3acb979b
TJ Hollis <thewelshaussie@hotmail.com>
2020-11-23 17:47:10 +0000

    schema returns

--- db/schema.rb
+++ db/schema.rb
@@ -17,5 +17,100 @@ ActiveRecord::Schema.define(version: 2020_11_23_160827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "pot_id", null: false
+    t.index ["pot_id"], name: "index_chatrooms_on_pot_id"
+  end
 
+  create_table "contributions", force: :cascade do |t|
+    t.string "contribution_sku"
+    t.integer "amount_cents", default: 0, null: false
+    t.string "session_id"
+    t.bigint "user_id", null: false
+    t.bigint "pot_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pot_id"], name: "index_contributions_on_pot_id"
+    t.index ["user_id"], name: "index_contributions_on_user_id"
+  end
+
+  create_table "gift_votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "gift_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gift_id"], name: "index_gift_votes_on_gift_id"
+    t.index ["user_id"], name: "index_gift_votes_on_user_id"
+  end
+
+  create_table "gifts", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "pot_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "price", default: 0
+    t.index ["pot_id"], name: "index_gifts_on_pot_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "pots", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description"
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "min_amount", default: 1, null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "total_amount", default: 0
+    t.index ["user_id"], name: "index_pots_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "nickname"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users_pots", force: :cascade do |t|
+    t.bigint "pot_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pot_id"], name: "index_users_pots_on_pot_id"
+    t.index ["user_id"], name: "index_users_pots_on_user_id"
+  end
+
+  add_foreign_key "chatrooms", "pots"
+  add_foreign_key "contributions", "pots"
+  add_foreign_key "contributions", "users"
+  add_foreign_key "gift_votes", "gifts"
+  add_foreign_key "gift_votes", "users"
+  add_foreign_key "gifts", "pots"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
+  add_foreign_key "pots", "users"
+  add_foreign_key "users_pots", "pots"
+  add_foreign_key "users_pots", "users"
 end
