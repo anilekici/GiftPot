f0c1e3d7b9197367e3d4ae5c577e721751ede9c5
anil ekici <anilcanekici@gmail.com>
2020-11-23 12:31:26 +0000

    chat created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_123416) do
+ActiveRecord::Schema.define(version: 2020_11_23_110243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "gift_votes", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -30,6 +36,16 @@ ActiveRecord::Schema.define(version: 2020_11_20_123416) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["pot_id"], name: "index_gifts_on_pot_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "pots", force: :cascade do |t|
@@ -54,6 +70,7 @@ ActiveRecord::Schema.define(version: 2020_11_20_123416) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -70,6 +87,8 @@ ActiveRecord::Schema.define(version: 2020_11_20_123416) do
   add_foreign_key "gift_votes", "gifts"
   add_foreign_key "gift_votes", "users"
   add_foreign_key "gifts", "pots"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "pots", "users"
   add_foreign_key "users_pots", "pots"
   add_foreign_key "users_pots", "users"
